ee42000a78e56dd0f9a856965e5d4d02323f9db9
Kelvin Brito <britok30@gmail.com>
2022-11-02 10:37:24 -0400

    add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.blognlp.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  // ...other options
+};
