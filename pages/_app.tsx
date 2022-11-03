4261ae09786f2f806370b80d266e0bf6a387a3ce
Kelvin Brito <britok30@gmail.com>
2022-11-03 11:08:42 -0400

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -28,6 +29,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         crossOrigin="anonymous"
       />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
